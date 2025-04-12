7b365af79041728eeb4d3b0a806dd336f6074fd2
Abdulazeez <azeezmuritalaadeniyi@gmail.com>
2025-04-12 23:19:42 +0100

    added add Book controller Functionality

---
+++ src/main/java/caverde/task/library/management/controller/BookController.java
@@ -0,0 +1,30 @@
+package caverde.task.library.management.controller;
+
+import caverde.task.library.management.dto.request.AddBookRequest;
+import caverde.task.library.management.dto.response.AddBookResponse;
+import caverde.task.library.management.dto.response.ApiResponse;
+import caverde.task.library.management.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    private final BookService bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    @PostMapping("/addBook")
+    public ResponseEntity<?> addBook(@RequestBody AddBookRequest addBookRequest) {
+        try{
+            AddBookResponse addBookResponse = bookService.addBook(addBookRequest);
+            return new ResponseEntity<>(new ApiResponse(true,addBookResponse), HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+}
