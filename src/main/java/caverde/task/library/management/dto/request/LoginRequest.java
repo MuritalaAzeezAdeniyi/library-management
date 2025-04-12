42d3ab5217f5867c122ca3d20a1ef53e68081d97
Abdulazeez <azeezmuritalaadeniyi@gmail.com>
2025-04-12 14:58:28 +0100

    added userDTO classes

---
+++ src/main/java/caverde/task/library/management/dto/request/LoginRequest.java
@@ -0,0 +1,6 @@
+package caverde.task.library.management.dto.request;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/caverde/task/library/management/dto/request/RegisterUserRequest.java
@@ -0,0 +1,31 @@
+package caverde.task.library.management.dto.request;
+
+public class RegisterUserRequest {
+    private String firstName;
+    private String password;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
